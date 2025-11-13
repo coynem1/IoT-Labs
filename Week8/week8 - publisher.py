6d4523fd002188cea852a41a6fba415642628385
george-crossan <georgecrossan9@gmail.com>
2025-11-13 07:54:52 +0000

    updated publisher code

--- Week8/week8 - publisher.py
+++ Week8/week8 - publisher.py
@@ -1,8 +1,49 @@
 import umqtt.robust as umqtt
+import machine
+from network import WLAN
+import time
 #Assuming that you connect to the internet as normal...
 
-HOSTNAME = 'Raspberry Pi IP'
-PORT = 1883
+temp_sensor = machine.ADC(4)
+timer = machine.Timer()
+
+def connect(
+    wifi_obj,
+    ssid,
+    password,
+    timeout=10
+):
+    wifi_obj.connect(ssid, password)
+
+    # Check for connection until timeout
+    while timeout > 0:
+        if wifi_obj.status() != 3:
+            time.sleep(1)
+            timeout -= 1
+        else:
+            print(f'IP: {wifi_obj.ifconfig()}')
+            return True
+    return False
+
+def read_temp(t):
+    # Gets temperature
+    value = temp_sensor.read_u16()
+    voltage = value * (3.3 / 2 ** 16)
+    temperature = 27 - (voltage - 0.706) / .001721
+    
+    print(f'The temperature is {temperature} degrees')
+    mqtt.publish(TOPIC, str(temperature).encode())
+
+ssid = 'Galaxy S22U'
+password = 'georgepassword'
+
+wifi = WLAN(WLAN.IF_STA)
+wifi.active(True)
+
+connect(wifi, ssid, password)
+
+HOSTNAME = '192.168.101.105'
+PORT = 8080
 TOPIC= 'temp/pico'
 
 mqtt = umqtt.MQTTClient(
@@ -14,6 +55,8 @@ mqtt = umqtt.MQTTClient(
 
 mqtt.connect()
 
+timer.init(freq=1, mode=machine.Timer.PERIODIC, callback=read_temp)
+
+
 #Assuming that you have the temperature as an int or a
 #float in a variable called `temp':
-mqtt.publish(TOPIC, str(temp).encode())
